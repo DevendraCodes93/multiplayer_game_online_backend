5cace035d0ae1cb21c7d888d65eb989153cb4b5d
DevendraCodes93 <devu935352@gmail.com>
2025-07-05 13:44:14 +0530

    Initial commit for Grid Wars frontend

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import cors from "cors";
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
 app.use("/api/game", gameRouter);
 
 server.listen(3001, () => {
